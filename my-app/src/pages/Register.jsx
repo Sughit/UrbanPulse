6b969d53747019656b4d140b28640b956bc9395b
Sughit <andreisorici2007@gmail.com>
2026-03-04 15:27:28 +0200

    .

--- my-app/src/pages/Register.jsx
+++ my-app/src/pages/Register.jsx
@@ -1,4 +1,3 @@
-// src/pages/Register.jsx
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
